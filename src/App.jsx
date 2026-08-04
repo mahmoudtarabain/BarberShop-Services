02d482cc387c39e16044c5d994d1cc0fce89b30d
unknown <mahmoudtarrabain516@gmail.com>
2026-08-04 15:10:03 +0200

    Document Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// Main App component with routing
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+// Navigation bar with mobile menu
 import { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
